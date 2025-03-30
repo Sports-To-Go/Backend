08f5bab05db4c55e72baac5a05a4a10a86d6e1a1
Bogdan Ciornohac <bogdanciornohac@gmail.com>
2025-03-30 22:22:00 +0300

    db connection

---
+++ src/main/java/org/sportstogo/backend/Demo.java
@@ -0,0 +1,15 @@
+package org.sportstogo.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Demo {
+    @Id
+    public int id;
+
+}
---
+++ src/main/java/org/sportstogo/backend/DemoRepository.java
@@ -0,0 +1,9 @@
+package org.sportstogo.backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DemoRepository extends JpaRepository<Demo,Integer> {
+
+}
