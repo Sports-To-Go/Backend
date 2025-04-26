b9a08817fe88f0362fb33a55af2a487cc84a3d60
SmauRobert <robertgeorgesmau@gmail.com>
2025-04-27 02:10:29 +0300

    Integrated Frontend with Backend. Modified User table. Now all generic user data is on the firebase db and the app's specific data is on this backend.

--- src/main/java/org/sportstogo/backend/Models/User.java
+++ src/main/java/org/sportstogo/backend/Models/User.java
@@ -1,12 +1,9 @@
 package org.sportstogo.backend.Models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 
 @Entity
 @Table(name="Users")
