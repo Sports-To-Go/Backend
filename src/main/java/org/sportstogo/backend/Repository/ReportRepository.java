cfb50255d5ecdaa076003459ec04e2d141ee959a
Bogdan Ciornohac <bogdanciornohac@gmail.com>
2025-05-28 23:14:13 +0300

    small fixes

--- src/main/java/org/sportstogo/backend/Repository/ReportRepository.java
+++ src/main/java/org/sportstogo/backend/Repository/ReportRepository.java
@@ -1,8 +1,14 @@
 package org.sportstogo.backend.Repository;
 
+import jakarta.transaction.Transactional;
 import org.sportstogo.backend.Models.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReportRepository extends JpaRepository<Report,Long> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Report r WHERE r.targetId = :targetId")
     void deleteByTargetId(String targetId);
 }
