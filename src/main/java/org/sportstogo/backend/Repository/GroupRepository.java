6f8ec685dcb18238e4b5b96fed9f0debe29fe9b8
onofreiradu <onofreir54@gmail.com>
2025-05-28 20:24:44 +0300

    fixes

--- src/main/java/org/sportstogo/backend/Repository/GroupRepository.java
+++ src/main/java/org/sportstogo/backend/Repository/GroupRepository.java
@@ -15,45 +15,23 @@ import java.util.List;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     @Query(value = """
-        SELECT g.id AS id, g.name AS name,\s
-               CAST(COUNT(DISTINCT gm.user_id) AS INT) AS memberCount,\s
-               COALESCE(m.content, 'No messages yet') AS description
+        SELECT g.id AS id,
+               g.name AS name,
+               COALESCE(g.description, 'No description') AS description
         FROM groups g
-        INNER JOIN group_memberships gm ON g.id = gm.group_id
-        LEFT JOIN (
-            SELECT m1.group_id, m1.content
-            FROM messages m1
-            INNER JOIN (
-                SELECT group_id, MAX(time_sent) AS max_time_sent
-                FROM messages
-                GROUP BY group_id
-            ) m2 ON m1.group_id = m2.group_id AND m1.time_sent = m2.max_time_sent
-        ) m ON g.id = m.group_id
-        WHERE g.id IN (
+        LEFT JOIN group_memberships gm ON g.id = gm.group_id
+        WHERE g.id NOT IN (
             SELECT group_id
             FROM group_memberships
             WHERE user_id = :uid
         )
-        GROUP BY g.id, g.name, m.content
-       \s""", nativeQuery = true)
-    List<GroupPreviewDTO> findChatPreviewsByUserId(@Param("uid") String uid);
-
-    @Query(value = """
-    SELECT g.id AS id,
-           g.name AS name,
-           COUNT(DISTINCT gm.user_id)::int AS memberCount,
-           COALESCE(g.description, 'No description') AS description
-    FROM groups g
-    LEFT JOIN group_memberships gm ON g.id = gm.group_id
-    WHERE g.id NOT IN (
-        SELECT group_id FROM group_memberships WHERE user_id = :uid
-    )
-    AND g.id NOT IN (
-        SELECT group_id FROM join_requests WHERE user_id = :uid
-    )
-    GROUP BY g.id, g.name, g.description
-    ORDER BY memberCount DESC
-    """, nativeQuery = true)
+        AND g.id NOT IN (
+            SELECT group_id
+            FROM join_requests
+            WHERE user_id = :uid
+        )
+        GROUP BY g.id, g.name, g.description
+   \s""", nativeQuery = true)
     List<GroupPreviewDTO> findGroupRecommendations(@Param("uid") String uid);
 
     @Modifying
